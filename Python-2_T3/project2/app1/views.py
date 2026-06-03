098cd0065e615a4775396cc9fc67a34b4667d277
AlishaPandey21 <pandeyalisha2112@gmail.com>
2026-06-03 09:40:41 +0530

    Add files via upload

---
+++ Python-2_T3/project2/app1/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def about(request):
+    return render(request,'about.html')
+def course(request):
+    return render(request,'course.html')
+def home(request):
+    return render(request,'home.html')
+def java(request):
+    return render(request,'java.html')
+def python(request):
+    return render(request,'python.html')
---
+++ Python-2_T3/project3/app1/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from app1.views import home,about,course
+
+urlpatterns=[
+    path('',home),
+    path('about/',about,name="abouts"),
+    path('course/',course,name="courses"),
+]
