bbf1474714cc3a4b3314e1f06491e2a4948b6f5e
AlishaPandey21 <pandeyalisha2112@gmail.com>
2026-06-03 09:44:57 +0530

    Delete Python-2_T3/project3 directory

--- Python-2_T3/project3/app1/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from app1.views import home,about,course
-
-urlpatterns=[
-    path('',home),
-    path('about/',about,name="abouts"),
-    path('course/',course,name="courses"),
-]
